fa53eb18d32910d2665e9ec5381f25a60263c1ab
Ironman680-0202 <sandeep.chandra52361234@gmail.com>
2026-05-05 12:00:49 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD PoC Application is running successfully on Feature');
+  res.send('CI/CD PoC Application is running successfully on Feature yo yo');
 });
 
-// Start server ONLY when running app directly
+
 if (require.main === module) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
